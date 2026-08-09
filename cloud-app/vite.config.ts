693e6815a8aa58007ab19ca718328f446cb8ead8
unknown <your-email@example.com>
2026-08-09 05:32:27 -1100

    widen build target for older mobile browser support

--- cloud-app/vite.config.ts
+++ cloud-app/vite.config.ts
@@ -1,3 +1,15 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(), tailwindcss()
+//   ],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
@@ -5,6 +17,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(), tailwindcss()
+    react(),
+    tailwindcss()
   ],
+  build: {
+    target: 'es2015',
+  },
 })
